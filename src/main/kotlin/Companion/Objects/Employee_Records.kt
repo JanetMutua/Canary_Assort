aebb03779f685efda846a5fd4fe1a83a7efd09ac
Janet <janetmutua04@gmail.com>
2022-11-09 09:46:37 +0300

    Feat:companion objects|singletons

---
+++ src/main/kotlin/Companion/Objects/Employee_Records.kt
@@ -0,0 +1,76 @@
+@file:JvmName("Employees")
+
+package Companion.Objects
+
+fun main(args: Array<String>){
+    var Jordan = Employee(234, "Jordan", 23)
+    Jordan.displayDetails()
+
+    Jordan.addJobDesription("Accountant")
+
+    Employee.addPostGrad("Jordan")
+    Employee.addUndergrad("Linus")
+
+    println(Employee.addUndergrad("Agnes"))
+
+}
+
+class Implementer{
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>){
+            var Jordan = Employee(234, "Jordan", 23)
+            Jordan.displayDetails()
+
+            Jordan.addJobDesription("Accountant")
+
+            Employee.addPostGrad("Jordan")
+            Employee.addUndergrad("Linus")
+
+            println(Employee.addUndergrad("Agnes"))
+
+        }
+    }
+}
+
+
+open class Employee(var id:Int = 0, var name:String, var age:Int = 23){
+
+    fun displayDetails(){
+        println("Employee records: $name , $id , $age ")
+    }
+
+    fun addJobDesription(jobType:String){
+        val Job = EmployeeJob.allJobDescriptions
+            .filter { it.jobName == jobType }
+            .firstOrNull()
+
+        println(Job)
+
+    }
+    // defining factory methods using the companion object
+    companion object : XmlSerializer<Employee>{
+        override fun toXml(item: Employee){
+
+        }
+
+        fun addUndergrad(name: String): Undergrad{
+            return Undergrad(name)
+        }
+
+        fun addPostGrad(name: String): Postgraduate{
+            return Postgraduate(name)
+        }
+    }
+
+
+}
+// classes to be used for factory methods with a companion object
+class Undergrad(degreeName: String): Employee(name = ""){}
+class Postgraduate(postDegreeName: String) : Employee(name = ""){}
+
+// interface to be implemented by the companion object
+
+interface XmlSerializer<T>{
+    fun toXml(item:T)
+}
--- src/main/kotlin/Companion/Objects/object_keyword.kt
+++ src/main/kotlin/Companion/Objects/object_keyword.kt
@@ -1,2 +1,14 @@
 package Companion.Objects
 
+open class JobDescription(var jobName:String){
+
+}
+
+// creates a singleton
+object EmployeeJob{
+    var allJobDescriptions = arrayListOf<JobDescription>()
+
+    fun initialize(){
+        allJobDescriptions.add(JobDescription("Accountant"))
+    }
+}
